ebb95702e6eec284dbefcbf9d93e3006e9559a51
LuffiDeMonk <thapaprabhat4@gmail.com>
2024-01-21 13:37:15 -0800

    couse filter  added

--- components/stream/StreamContainer.tsx
+++ components/stream/StreamContainer.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useMemo, useState } from 'react'
 import StreamCard from './StreamCard'
 import { streams } from '@/constant'
 import Error from '../Warning'
@@ -19,7 +21,8 @@ type StreamData = {
 //     return response.json()
 // }
 
-export default async function StreamContainer({ universityName }: { universityName: string[] }) {
+export default function StreamContainer({ universityName }: { universityName: string[] }) {
+    const [search, setSearch] = useState('')
     // const streamData: StreamData[] = await getStreamData(universityName[0])
     // if (streamData.length === 0) {
     //     return <h2>No data</h2>
@@ -28,31 +31,32 @@ export default async function StreamContainer({ universityName }: { universityNa
     let filteredData = streams.filter((item) => item.universityName.toLowerCase() === universityName[0].replaceAll("%20", " ").toLowerCase())
 
 
-    if (filteredData.length === 0) {
-        return (
-            <Error />
-        )
-    }
+    let filteredCourses = useMemo(() => {
+        return filteredData[0].streams.filter(item => item.toLowerCase().includes(search.toLowerCase()))
+
+    }, [search, filteredData])
+
 
     return (
         <>
             <div className='flex flex-col gap-3 md:gap-0 md:flex-row md:items-center md:justify-between'>
                 <h1 className='text-xl font-semibold text-green-500'>Faculty</h1>
-                <form>
-                    <input type="text" placeholder='Search for available courses' className='px-5 py-2 rounded-full w-full md:w-80 border-2 border-gray-300 outline-none focus:outline-none' />
-                </form>
+                <input onChange={e => setSearch(e.target.value)} type="text" placeholder='Search for available courses' className='px-5 py-2 rounded-full w-full md:w-80 border-2 border-gray-300 outline-none focus:outline-none' />
             </div>
-            <div className="min-h-[50vh]">
-                <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 mb-8">
-                    {
-                        filteredData[0]?.streams.map((item, idx) => (
-                            <React.Fragment key={idx}>
-                                <StreamCard stream={item} />
-                            </React.Fragment>
-                        ))
-                    }
+            <div className="min-h-[60vh]">
+                {filteredCourses.length !== 0 ? (<>
+                    <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 mb-8">
+                        {
+                            filteredCourses.map((item, idx) => (
+                                <React.Fragment key={idx}>
+                                    <StreamCard stream={item} />
+                                </React.Fragment>
+                            ))
+                        }
 
-                </div>
+                    </div>
+                </>) : (<Error height='h-[60vh]' />)}
+
             </div>
         </>
     )
